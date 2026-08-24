4e549f52cd0e9ffe2ca828724a30b74924ce0efe
tobyaaa <68621460+tobyaaaa@users.noreply.github.com>
2026-08-24 16:06:55 -0400

    frlog animations

--- assets/scripts/frog.gd
+++ assets/scripts/frog.gd
@@ -1,16 +1,18 @@
 extends Moveable
 class_name Frog
 
+func _ready() -> void:
+	$FrogSprite.play("idle_down")
 #to be run after player movement is calculated
 func do_frog_movement(direction:Vector2i):
-	#if tile - player_tile == Vector2i.DOWN:
-	#	level.try_move(self, Vector2i.DOWN)
-	#if tile - player_tile == Vector2i.UP:
-	#	level.try_move(self, Vector2i.UP)
-	#if tile - player_tile == Vector2i.LEFT:
-	#	level.try_move(self, Vector2i.LEFT)
-	#if tile - player_tile == Vector2i.RIGHT:
-	
+	if direction == Vector2i.DOWN:
+		$FrogSprite.animation = "idle_down"
+	if direction == Vector2i.UP:
+		$FrogSprite.animation = "idle_up"
+	if direction == Vector2i.LEFT:
+		$FrogSprite.animation = "idle_left"
+	if direction == Vector2i.RIGHT:
+		$FrogSprite.animation = "idle_right"
 	level.try_move_frog(self, direction)
 	
 	#todo try_move isn't fantastic here because frogs don't move
