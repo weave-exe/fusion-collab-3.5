453b66bc6d41ae36cf6211ccf262364edd8bb743
mega weave <50036259+weave-exe@users.noreply.github.com>
2026-08-24 19:38:23 -0500

    bus
    grrrrrrrrr

--- assets/scripts/player.gd
+++ assets/scripts/player.gd
@@ -21,6 +21,7 @@ func _input(event: InputEvent) -> void:
 		update_player_audio("walking")
 	elif event.is_action_pressed("undo"):
 		level.undo()
+		update_player_audio("rewind")
 		return
 	
 	var frog = level.get_moveable_at_tile(tile + Vector2i.UP)
@@ -35,8 +36,7 @@ func _input(event: InputEvent) -> void:
 	frog = level.get_moveable_at_tile(tile + Vector2i.RIGHT)
 	if frog as Frog != null:
 		frog.do_frog_movement(Vector2i.RIGHT)
-		update_player_audio("rewind")
-		
+	
 func update_player_audio(audio_name: String):
 	if audio_name == "none":
 		player_audiostream.stop()
