02ead8ef6c03fd5eb17889d893fbbf37320914d3
ABuffZucchini <gagelannigan@gmail.com>
2026-08-26 17:50:31 -0400

    tutorial decals, level loader progress with zoomout

--- assets/scripts/core/level_manager.gd
+++ assets/scripts/core/level_manager.gd
@@ -1,13 +1,13 @@
 extends Node
 
-signal level_loaded(level: Level)
+signal level_loaded(level: Level, zoom: int)
 
 var container: Node
-var levels: Array[PackedScene] = []
+var levels: Array[Level_Resource] = []
 var level_index := -1
 var current_level: Level
 
-func setup(_container: Node, _levels: Array[PackedScene]) -> void:
+func setup(_container: Node, _levels: Array[Level_Resource]) -> void:
 	container = _container
 	levels = _levels
 	
@@ -22,9 +22,12 @@ func load_level(i: int) -> void:
 		
 	# add new level
 	level_index = i
-	current_level = levels[i].instantiate() as Level
+	current_level = levels[i].scene.instantiate() as Level
 	container.add_child(current_level)
-	level_loaded.emit(current_level)
+	level_loaded.emit(current_level, levels[i].zoom)
+	# set zoom
+	
+	
 
 func load_next() -> void:
 	load_level(level_index + 1)
