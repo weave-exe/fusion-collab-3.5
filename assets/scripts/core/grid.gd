4f0fa5678bae1615c1aaacde9f78fb7680054675
mega weave <50036259+weave-exe@users.noreply.github.com>
2026-08-25 20:35:52 -0500

    progress!
    new decal tilemaplayer, stuff started for shrooms

--- assets/scripts/core/grid.gd
+++ assets/scripts/core/grid.gd
@@ -3,10 +3,12 @@ extends RefCounted
 class_name Grid
 
 var terrain: TileMapLayer
+var decals: TileMapLayer
 
-func _init(_terrain: TileMapLayer) -> void:
+func _init(_terrain: TileMapLayer, _decals: TileMapLayer) -> void:
 	terrain = _terrain
-	
+	decals = _decals
+
 func tile_to_world(tile_pos: Vector2i) -> Vector2: 
 	return terrain.to_global(terrain.map_to_local(tile_pos))
 
@@ -19,5 +21,8 @@ func is_blocking_player(tile: Vector2i) -> bool:
 		return true
 	return data.get_custom_data("block_player")
 	
-func is_frog_block():
-	pass
+func is_blocking_frog(tile: Vector2i) -> bool:
+	var mush_data := decals.get_cell_tile_data(tile)
+	if mush_data == null:
+		return true
+	return mush_data.get_custom_data("block_frog")
