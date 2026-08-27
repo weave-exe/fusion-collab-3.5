d1ca2b4547be03446de56dcbaa0b2a6634c98051
ricky0606 <98306742+ricky0606@users.noreply.github.com>
2026-08-26 22:31:17 -0700

    tweets per level via level_resource

--- assets/scripts/core/level_manager.gd
+++ assets/scripts/core/level_manager.gd
@@ -3,16 +3,17 @@ extends Node
 signal level_loaded(level: Level, zoom: int)
 
 var container: Node
-var levels: Array[Level_Resource] = []
+var levels: Array[LevelResource] = []
 var level_index := -1
 var current_level: Level
+var current_level_resource: LevelResource
 
-func setup(_container: Node, _levels: Array[Level_Resource]) -> void:
+func setup(_container: Node, _levels: Array[LevelResource]) -> void:
 	container = _container
 	levels = _levels
 	
 func load_level(i: int) -> void:
-	if i < 0 or i > levels.size():
+	if i < 0 or i >= levels.size():
 		return
 		
 	# remove level
@@ -22,12 +23,10 @@ func load_level(i: int) -> void:
 		
 	# add new level
 	level_index = i
-	current_level = levels[i].scene.instantiate() as Level
+	current_level_resource = levels[i]
+	current_level = current_level_resource.scene.instantiate() as Level
 	container.add_child(current_level)
-	level_loaded.emit(current_level, levels[i].zoom)
-	# set zoom
-	
-	
+	level_loaded.emit(current_level, levels[i].zoom)	
 
 func load_next() -> void:
 	load_level(level_index + 1)
