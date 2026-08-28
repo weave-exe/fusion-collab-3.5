c3912738ae8bd69e27c75cdcaf7f164d8a900416
ABuffZucchini <gagelannigan@gmail.com>
2026-08-28 18:33:34 -0400

    i lied i just did ui stuff

--- assets/scripts/ui/menu_manager.gd
+++ assets/scripts/ui/menu_manager.gd
@@ -6,4 +6,5 @@ extends CanvasGroup
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	for i in level_buttons.size():
+		level_buttons[i].level_num=i+1
