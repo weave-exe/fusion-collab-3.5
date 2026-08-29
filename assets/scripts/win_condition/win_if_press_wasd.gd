b313e91e74f6d8ce04faa98dd2b5f737ca26d974
ABuffZucchini <gagelannigan@gmail.com>
2026-08-28 23:11:18 -0400

    gage levels one last time

---
+++ assets/scripts/win_condition/win_if_press_wasd.gd
@@ -0,0 +1,20 @@
+extends Node
+
+@export var timer: Timer
+
+func _input(event: InputEvent) -> void:
+
+	if event.is_action_pressed("move_up"):
+		timer.start()
+	elif event.is_action_pressed("move_left"):
+		timer.start()
+	elif event.is_action_pressed("move_right"):
+		timer.start()
+	elif event.is_action_pressed("move_down"):
+		timer.start()
+
+		
+
+
+func _on_timer_timeout() -> void:
+	SignalBus.level_won.emit()
